07a32f1d710dfebd358785097396ddefb008f7cc
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-04-26 13:31:28 -0300

    layout pagamento

--- ElginTefApiVBNET/Pagamento.xaml.vb
+++ ElginTefApiVBNET/Pagamento.xaml.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Pagamento
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
 
+    End Sub
+
+    Private Sub BtnIniciarOperacaoTEF_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub BtnIniciarOperacaoPIX_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub BtnBS_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
 End Class
