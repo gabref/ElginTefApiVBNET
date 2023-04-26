95fd3103227dce287d95b177c276ba2114680a1b
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-04-26 15:02:43 -0300

    pagamento tef e pix

--- ElginTefApiVBNET/ControleApi.vb
+++ ElginTefApiVBNET/ControleApi.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.InteropServices
+Imports Newtonsoft.Json
 
 Public Class ControleApi
     Public Shared Property ModoOperacao As String = String.Empty
@@ -52,6 +53,89 @@ Public Class ControleApi
 
     <DllImport(PATH, CallingConvention:=CallingConvention.StdCall)>
     Friend Shared Function FinalizarOperacaoTEF(id As Integer) As IntPtr
+    End Function
+
+
+
+    ' //===================================================================== //
+    ' //============ METODOS UTILITÁRIOS PARA O EXEMPLO C# ================== //
+    ' //===================================================================== //
+
+    Public Shared Function incrementarSequencial(ByVal sequencial As String) As String
+        Dim ok As Boolean
+        Dim value As Double = Nothing
+        ok = Double.TryParse(sequencial, value) AndAlso Not Double.IsNaN(value) AndAlso Not Double.IsInfinity(value)
+        If Not ok Then Return String.Empty
+        value += 1
+        Return value.ToString()
+    End Function
+
+    Public Shared Function getRetorno(ByVal resp As String) As String
+        Dim _jsonDic As IDictionary(Of String, Object) = jsonify(resp)
+        Return getStringValue(_jsonDic, "tef", "retorno")
+    End Function
+
+    Public Shared Function getSequencial(ByVal resp As String) As String
+        Dim _jsonDic As IDictionary(Of String, Object) = jsonify(resp)
+        Return getStringValue(_jsonDic, "tef", "sequencial")
+    End Function
+
+    Public Shared Function getComprovante(ByVal resp As String, ByVal via As String) As String
+        If via = "loja" Then
+            Dim _jsonDic As IDictionary(Of String, Object) = jsonify(resp)
+            Return getStringValue(_jsonDic, "tef", "comprovanteDiferenciadoLoja")
+        ElseIf via = "cliente" Then
+            Dim _jsonDic As IDictionary(Of String, Object) = jsonify(resp)
+            Return getStringValue(_jsonDic, "tef", "comprovanteDiferenciadoPortador")
+        Else
+            Return String.Empty
+        End If
+    End Function
+
+    Public Shared Function jsonify(ByVal jsonString As String) As IDictionary(Of String, Object)
+        If jsonString IsNot Nothing Then
+            Dim _Dic As IDictionary(Of String, Object) = JsonConvert.DeserializeObject(Of Dictionary(Of String, Object))(jsonString)
+            Return _Dic
+        Else
+            Dim _Dic As IDictionary(Of String, Object) = New Dictionary(Of String, Object)()
+            Return _Dic
+        End If
+    End Function
+
+    Public Shared Function stringify(ByVal _jsonDic As IDictionary(Of String, Object)) As String
+        Return JsonConvert.SerializeObject(_jsonDic, Formatting.Indented)
+    End Function
+
+    Public Shared Function getStringValue(ByVal dicJson As IDictionary(Of String, Object), ByVal keyOut As String, ByVal keyIn As String) As String
+        Dim value As Object = Nothing, valueIn As Object = Nothing
+
+        If dicJson IsNot Nothing AndAlso dicJson.TryGetValue(keyOut, value) Then
+            Dim _Dic As IDictionary(Of String, Object) = value.ToObject(Of Dictionary(Of String, Object))()
+
+            If _Dic IsNot Nothing AndAlso _Dic.TryGetValue(keyIn, valueIn) Then
+                Console.WriteLine(valueIn.[GetType]())
+                Return valueIn.ToString()
+            Else
+                Return String.Empty
+            End If
+        Else
+            Return String.Empty
+        End If
+    End Function
+
+    Public Shared Function getStringValue(ByVal dicJson As IDictionary(Of String, Object), ByVal key As String) As String
+        Dim value As Object = Nothing
+
+        If dicJson IsNot Nothing AndAlso dicJson.TryGetValue(key, value) Then
+            Return value.ToString()
+        Else
+            Return String.Empty
+        End If
+    End Function
+
+    Public Shared Function __Function() As String
+        Dim stackTrace As StackTrace = New StackTrace()
+        Return stackTrace.GetFrame(1).GetMethod().Name
     End Function
 
 End Class
--- ElginTefApiVBNET/Pagamento.xaml.vb
+++ ElginTefApiVBNET/Pagamento.xaml.vb
@@ -1,6 +1,11 @@
-﻿Imports System.Text.RegularExpressions
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Text.RegularExpressions
+Imports System.Threading
 
 Public Class Pagamento
+
+    Public clickEvent As AutoResetEvent = New AutoResetEvent(False)
 
     Private Const ADM_USUARIO As String = ""
     Private Const ADM_SENHA As String = ""
@@ -53,19 +58,402 @@ Public Class Pagamento
         LblValor.Text = "0.00"
     End Sub
 
-    Private Sub BtnIniciarOperacaoTEF_Click(sender As Object, e As RoutedEventArgs)
+    ' UI
 
+    Private Async Sub BtnIniciarOperacaoTEF_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Operacao = OPERACAO_TEF
+        lblOperador1.Visibility = Visibility.Visible
+        lblOperador1.Content = "AGUARDE..."
+        valorTotal = LblValor.Text
+        LblValor.Text = ""
+        btnInciarOperacaoPIX.IsEnabled = False
+        btnIniciarOperacaoTEF.IsEnabled = False
+        Await Task.Run(Sub()
+                           TesteApiElginTEF()
+                       End Sub)
+        btnInciarOperacaoPIX.IsEnabled = True
+        btnIniciarOperacaoTEF.IsEnabled = True
     End Sub
 
-    Private Sub BtnIniciarOperacaoPIX_Click(sender As Object, e As RoutedEventArgs)
-
+    Private Async Sub BtnIniciarOperacaoPIX_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Operacao = OPERACAO_PIX
+        lblOperador1.Visibility = Visibility.Visible
+        lblOperador1.Content = "AGUARDE..."
+        valorTotal = LblValor.Text
+        LblValor.Text = ""
+        btnInciarOperacaoPIX.IsEnabled = False
+        btnIniciarOperacaoTEF.IsEnabled = False
+        Await Task.Run(Sub()
+                           TesteApiElginTEF()
+                       End Sub)
+        btnInciarOperacaoPIX.IsEnabled = True
+        btnIniciarOperacaoTEF.IsEnabled = True
     End Sub
 
-    Private Sub btnOk_Click(sender As Object, e As RoutedEventArgs)
+    Private Sub btnOk_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        RetornoUI = ""
+        Dim retCmb As String = cmbLista.SelectedIndex.ToString()
+        Dim retTxt As String = txtOperador.Text
+        txtOperador.Text = ""
+        lblOperador1.Visibility = Visibility.Hidden
+        txtOperador.Visibility = Visibility.Hidden
+        btnOk.Visibility = Visibility.Hidden
+        btnCancelar.Visibility = Visibility.Hidden
+        txtOperador.Visibility = Visibility.Hidden
+        imgQRCode.Visibility = Visibility.Hidden
 
+        If cmbLista.Visibility = Visibility.Visible Then
+            RetornoUI = retCmb
+        Else
+            RetornoUI = retTxt
+        End If
+
+        cmbLista.Visibility = Visibility.Hidden
+        clickEvent.[Set]()
     End Sub
 
-    Private Sub btnCancelar_Click(sender As Object, e As RoutedEventArgs)
-
+    Private Sub btnCancelar_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        RetornoUI = "0"
+        cancelarColeta = "9"
+        clickEvent.[Set]()
     End Sub
+
+    Public Sub printUi(ByVal msg As String)
+        Dim unused = Dispatcher.Invoke(Function()
+                                           lblOperador1.Visibility = Visibility.Hidden
+                                           txtOperador.Visibility = Visibility.Hidden
+                                           btnOk.Visibility = Visibility.Hidden
+                                           btnCancelar.Visibility = Visibility.Hidden
+                                           txtOperador.Visibility = Visibility.Hidden
+                                           imgQRCode.Visibility = Visibility.Hidden
+
+                                           If msg.Contains("QRCODE;") Then
+                                               Dim start As Integer = msg.IndexOf(";") + 1
+                                               Dim [end] As Integer = msg.LastIndexOf(";")
+                                               Dim hexQrCodeString As String = msg.Substring(start, [end] - start)
+                                               ShowQRCode(hexQrCodeString)
+                                               imgQRCode.Visibility = Visibility.Visible
+                                               btnOk.Visibility = Visibility.Visible
+                                               btnCancelar.Visibility = Visibility.Visible
+                                           Else
+                                               lblOperador1.Content = msg
+                                               lblOperador1.Visibility = Visibility.Visible
+                                               Dim msgArray As String() = {"aguarde", "finalizada", "passagem", "cancelada", "iniciando confirmação"}
+
+                                               If Not msgArray.Any(AddressOf msg.ToLower().Contains) Then
+                                                   txtOperador.Visibility = Visibility.Visible
+                                                   txtOperador.Focus()
+                                                   btnOk.Visibility = Visibility.Visible
+                                                   btnCancelar.Visibility = Visibility.Visible
+                                               End If
+                                           End If
+                                       End Function)
+    End Sub
+
+    Public Sub printUi(ByVal elements As String())
+        Dispatcher.Invoke(Function()
+                              cmbLista.Items.Clear()
+                              lblOperador1.Visibility = Visibility.Hidden
+                              txtOperador.Visibility = Visibility.Hidden
+                              btnOk.Visibility = Visibility.Hidden
+                              btnCancelar.Visibility = Visibility.Hidden
+                              txtOperador.Visibility = Visibility.Hidden
+                              imgQRCode.Visibility = Visibility.Hidden
+                              lblOperador1.Visibility = Visibility.Visible
+                              btnCancelar.Visibility = Visibility.Visible
+                              btnOk.Visibility = Visibility.Visible
+
+                              For Each item As String In elements
+                                  cmbLista.Items.Add(item)
+                              Next
+
+                              cmbLista.Items.Add("Selecione uma opção")
+                              cmbLista.SelectedItem = "Selecione uma opção"
+                              cmbLista.Visibility = Visibility.Visible
+                          End Function)
+    End Sub
+
+    Public Sub ShowQRCode(ByVal hexString As String)
+        Dim imageBytes As Byte() = HexToByteArray(hexString)
+
+        Using stream As MemoryStream = New MemoryStream(imageBytes)
+            Dim qrImage As BitmapImage = New BitmapImage()
+            qrImage.BeginInit()
+            qrImage.StreamSource = stream
+            qrImage.CacheOption = BitmapCacheOption.OnLoad
+            qrImage.EndInit()
+            imgQRCode.Source = qrImage
+        End Using
+    End Sub
+
+    Private Function HexToByteArray(ByVal hexString As String) As Byte()
+        If hexString Is Nothing Then
+            Throw New ArgumentNullException("hexString")
+        End If
+
+        If hexString.Length Mod 2 <> 0 Then
+            Throw New ArgumentException("hexString must have an even length")
+        End If
+
+        Dim result As Byte() = New Byte(hexString.Length / 2 - 1) {}
+
+        For i As Integer = 0 To hexString.Length - 1 Step 2
+            Dim byteString As String = hexString.Substring(i, 2)
+            result(i / 2) = Convert.ToByte(byteString, 16)
+        Next
+
+        Return result
+    End Function
+
+    Public Sub WriteLogs(ByVal header As Boolean, ByVal logs As String, ByVal footer As Boolean)
+        Const div As String = vbLf & "==============================================" & vbLf
+        Dim _output As String = ""
+        If header Then _output += div
+        _output += logs
+        If footer Then _output += div
+        Dispatcher.Invoke(Function()
+                              LogsEntry.Text += _output & vbLf
+                              LogsEntry.ScrollToEnd()
+                          End Function)
+    End Sub
+
+    ' //===================================================================== //
+    ' //========================= LÓGICA DO TEF ============================= //
+    ' //===================================================================== //
+
+    ' //===================================================================== //
+    ' //=============================== TESTES ============================== //
+    ' //===================================================================== //
+
+    Public Sub TesteApiElginTEF()
+        Try
+            ControleApi.SetClientTCP("127.0.0.1", 60906)
+            ControleApi.ConfigurarDadosPDV("Meu PDV VB.NET", "v1.0.000", "Elgin", "01", "T0004")
+            Dim start As String = iniciar()
+            Dim retorno As String = ControleApi.getRetorno(start)
+
+            If retorno = String.Empty OrElse retorno <> "1" Then
+                finalizar()
+            End If
+
+            Dim sequencial As String = ControleApi.getSequencial(start)
+            sequencial = ControleApi.incrementarSequencial(sequencial)
+            Dim resp As String = String.Empty
+
+            If ControleApi.ModoOperacao.Contains("vender") Then
+                resp = If(Operacao = OPERACAO_TEF, vender(0, sequencial, OPERACAO_TEF), vender(0, sequencial, OPERACAO_PIX))
+            Else
+                resp = adm(0, sequencial)
+            End If
+
+            retorno = ControleApi.getRetorno(resp)
+
+            If retorno = String.Empty Then
+
+                If ControleApi.ModoOperacao.Contains("vender") Then
+                    resp = coletar(0, ControleApi.jsonify(resp))
+                Else
+                    resp = coletar(1, ControleApi.jsonify(resp))
+                End If
+
+                retorno = ControleApi.getRetorno(resp)
+            End If
+
+            If retorno = String.Empty Then
+                WriteLogs(True, "ERRO AO COLETAR DADOS", True)
+                Print("ERRO AO COLETAR DADOS")
+            ElseIf retorno = "0" Then
+                Dim comprovanteLoja As String = ControleApi.getComprovante(resp, "loja")
+                Dim comprovanteCliente As String = ControleApi.getComprovante(resp, "cliente")
+                WriteLogs(True, comprovanteLoja, True)
+                WriteLogs(True, comprovanteCliente, True)
+                WriteLogs(True, "TRANSAÇÃO OK, INICIANDO CONFIRMAÇÃO...", True)
+                Print("TRANSAÇÃO OK, INICIANDO CONFIRMAÇÃO...")
+                sequencial = ControleApi.getSequencial(resp)
+                Dim cnf As String = confirmar(sequencial)
+                retorno = ControleApi.getRetorno(cnf)
+
+                If retorno = String.Empty OrElse retorno <> "1" Then
+                    finalizar()
+                End If
+            ElseIf retorno = "1" Then
+                WriteLogs(True, "TRANSAÇÃO OK", True)
+                Print("TRANSAÇÃO OK")
+            Else
+                WriteLogs(True, "ERRO NA TRANSAÇÃO", True)
+                Print("ERRO NA TRANSAÇÃO")
+            End If
+
+            Dim [end] As String = finalizar()
+            retorno = ControleApi.getRetorno([end])
+
+            If retorno = String.Empty OrElse retorno <> "1" Then
+                finalizar()
+            End If
+
+        Catch __unusedDllNotFoundException1__ As DllNotFoundException
+            MessageBox.Show("Dll não encontrada")
+        End Try
+    End Sub
+
+
+    ' //===================================================================== //
+    ' //============ MÉTODOS PARA O CONTROLE DA TRANSAÇÃO (E1_TEF) ========== //
+    ' //===================================================================== //
+
+    Public Function iniciar() As String
+        Dim payload As IDictionary(Of String, Object) = New Dictionary(Of String, Object)()
+        Dim _intptr As IntPtr = ControleApi.IniciarOperacaoTEF(ControleApi.stringify(payload))
+        Dim start As String = Marshal.PtrToStringAnsi(_intptr)
+        WriteLogs(True, ControleApi.__Function() & "  " & start, True)
+        Return start
+    End Function
+
+    Public Function vender(ByVal cartao As Integer, ByVal sequencial As String, ByVal operacao As Integer) As String
+        WriteLogs(True, ControleApi.__Function() & " SEQUENCIAL UTILIZADO NA VENDA: " & sequencial, True)
+        Dim payload As IDictionary(Of String, Object) = New Dictionary(Of String, Object)()
+        payload.Add("sequencial", sequencial)
+
+        If valorTotal <> String.Empty Then
+            valorTotal = Regex.Replace(valorTotal, "[^\d]", "")
+            payload.Add("valorTotal", valorTotal)
+        End If
+
+        Dim pgto As String
+
+        If operacao = OPERACAO_TEF Then
+            Dim _intptr As IntPtr = ControleApi.RealizarPagamentoTEF(cartao, ControleApi.stringify(payload), True)
+            pgto = Marshal.PtrToStringAnsi(_intptr)
+        Else
+            Dim _intptr As IntPtr = ControleApi.RealizarPixTEF(ControleApi.stringify(payload), True)
+            pgto = Marshal.PtrToStringAnsi(_intptr)
+        End If
+
+        WriteLogs(True, ControleApi.__Function() & "  " & pgto, True)
+        Return pgto
+    End Function
+
+    Public Function adm(ByVal opcao As Integer, ByVal sequencial As String) As String
+        WriteLogs(True, ControleApi.__Function() & " SEQUENCIAL UTILIZADO NA VENDA: " & sequencial, True)
+        Dim payload As IDictionary(Of String, Object) = New Dictionary(Of String, Object)()
+        payload.Add("sequencial", sequencial)
+        Dim _intptr As IntPtr = ControleApi.RealizarAdmTEF(opcao, ControleApi.stringify(payload), True)
+        Dim admRed As String = Marshal.PtrToStringAnsi(_intptr)
+        WriteLogs(True, ControleApi.__Function() & "  " & admRed, True)
+        Return admRed
+    End Function
+
+    Public Function coletar(ByVal operacao As Integer, ByVal root As IDictionary(Of String, Object)) As String
+        Dim coletaRetorno, coletaSequencial, coletaMensagem, coletaTipo, coletaOpcao, coletaInformacao As String
+        coletaRetorno = ControleApi.getStringValue(root, "tef", "automacao_coleta_retorno")
+        coletaSequencial = ControleApi.getStringValue(root, "tef", "automacao_coleta_sequencial")
+        coletaMensagem = ControleApi.getStringValue(root, "tef", "mensagemResultado")
+        coletaTipo = ControleApi.getStringValue(root, "tef", "automacao_coleta_tipo")
+        coletaOpcao = ControleApi.getStringValue(root, "tef", "automacao_coleta_opcao")
+        WriteLogs(True, ControleApi.__Function() & " " & coletaMensagem.ToUpper(), True)
+        Print(coletaMensagem.ToUpper())
+        If coletaRetorno <> "0" Then Return ControleApi.stringify(root)
+        Dim payload As IDictionary(Of String, Object) = New Dictionary(Of String, Object)()
+        payload.Add("automacao_coleta_retorno", coletaRetorno)
+        payload.Add("automacao_coleta_sequencial", coletaSequencial)
+
+        If coletaTipo <> String.Empty AndAlso coletaOpcao = String.Empty Then
+            WriteLogs(True, "INFORME O VALOR SOLICITADO: ", True)
+            coletaInformacao = Read()
+
+            If cancelarColeta <> String.Empty Then
+                payload.Remove("automacao_coleta_retorno")
+                payload.Add("automacao_coleta_retorno", cancelarColeta)
+                cancelarColeta = String.Empty
+            End If
+
+            payload.Add("automacao_coleta_informacao", coletaInformacao)
+        ElseIf coletaTipo <> String.Empty AndAlso coletaOpcao <> String.Empty Then
+            Dim opcoes As String() = coletaOpcao.Split(Char.Parse(";"))
+            Dim elements As String() = New String(opcoes.Length - 1) {}
+
+            For i As Integer = 0 To opcoes.Length - 1
+                elements(i) += "[" & i & "] " & opcoes(i).ToUpper() & vbLf
+            Next
+
+            For i As Integer = 0 To opcoes.Length - 1
+                WriteLogs(False, "[" & i & "] " & opcoes(i).ToUpper() & vbLf, False)
+            Next
+
+            Print(elements)
+            WriteLogs(True, vbLf & "DIGITE A OPÇÂO DESEJADA: ", True)
+            coletaInformacao = opcoes(Integer.Parse(Read()))
+
+            If cancelarColeta <> String.Empty Then
+                payload.Remove("automacao_coleta_retorno")
+                payload.Add("automacao_coleta_retorno", cancelarColeta)
+                cancelarColeta = String.Empty
+            End If
+
+            payload.Add("automacao_coleta_informacao", coletaInformacao)
+        End If
+
+        Dim resp As String
+
+        If operacao = 1 Then
+            Dim _intptr As IntPtr = ControleApi.RealizarAdmTEF(0, ControleApi.stringify(payload), False)
+            resp = Marshal.PtrToStringAnsi(_intptr)
+        Else
+
+            If operacao = OPERACAO_PIX Then
+                Dim _intptr As IntPtr = ControleApi.RealizarPixTEF(ControleApi.stringify(payload), False)
+                resp = Marshal.PtrToStringAnsi(_intptr)
+            Else
+                Dim _intptr As IntPtr = ControleApi.RealizarPagamentoTEF(0, ControleApi.stringify(payload), False)
+                resp = Marshal.PtrToStringAnsi(_intptr)
+            End If
+        End If
+
+        Dim retorno As String = ControleApi.getRetorno(resp)
+
+        If retorno <> String.Empty Then
+            Return resp
+        End If
+
+        Return coletar(operacao, ControleApi.jsonify(resp))
+    End Function
+
+    Public Function confirmar(ByVal sequencial As String) As String
+        WriteLogs(True, ControleApi.__Function() & "SEQUENCIAL DA OPERAÇÃO A SER CONFIRMADA: " & sequencial, True)
+        Print("AGUARDE, CONFIRMANDO OPERAÇÃO...")
+        Dim _intptr As IntPtr = ControleApi.ConfirmarOperacaoTEF(Integer.Parse(sequencial), 1)
+        Dim cnf As String = Marshal.PtrToStringAnsi(_intptr)
+        WriteLogs(True, ControleApi.__Function() & cnf, True)
+        Return cnf
+    End Function
+
+    Public Function finalizar() As String
+        Dim _intptr As IntPtr = ControleApi.FinalizarOperacaoTEF(1)
+        Dim [end] As String = Marshal.PtrToStringAnsi(_intptr)
+        WriteLogs(True, ControleApi.__Function() & [end], True)
+        valorTotal = String.Empty
+        Print("OPERAÇÃO FINALIZADA!")
+        Return [end]
+    End Function
+
+
+
+    ' //===================================================================== //
+    ' //============ METODOS UTILITÁRIOS PARA O EXEMPLO C# ================== //
+    ' //===================================================================== //
+
+    Public Sub Print(ByVal msg As String)
+        printUi(msg)
+    End Sub
+
+    Public Sub Print(ByVal elements As String())
+        printUi(elements)
+    End Sub
+
+    Public Function Read() As String
+        clickEvent.WaitOne()
+        clickEvent.Reset()
+        Return RetornoUI
+    End Function
+
 End Class
